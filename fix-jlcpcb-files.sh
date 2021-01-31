c42caa1328decd34b8b2497cf83a3f04e1892d9d
Matti Airas <mairas@iki.fi>
2021-01-31 13:01:38 +0200

    Properly detect BOM source files in JLC fix script

--- fix-jlcpcb-files.sh
+++ fix-jlcpcb-files.sh
@@ -7,9 +7,10 @@ PROJ="SH-ESP32"
 DESTDIR="assembly"
 BOMFILE="${PROJ}_bom_jlc.csv"
 CPLFILE="${PROJ}_cpl_jlc.csv"
+BOM_SRC_FILE="${PROJ}.xml"
 
 # if we're ordering PCBs only, ignore the BOM and CPL parts
-if [ -f $BOMFILE ] ; then
+if [ -f $BOM_SRC_FILE ] ; then
     # fix the BOM and CPL files
     jlc-kicad-tools -n $PROJ -d cpl_rotations_db.csv .
 
